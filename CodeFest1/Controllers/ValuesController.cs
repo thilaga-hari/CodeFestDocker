48104105721ee4739a303657dc0812f8a627c7cc
thilaga-hari <thilaga.yuva@gmail.com>
2019-12-13 10:20:30 +0530

    code fest commit

---
+++ CodeFest1/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFest1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(double celsius)
+        {
+            double f;  
+            f = celsius * 9 / 5 + 32;  
+            return (f + "°F in Celsius is: " + celsius + "°C");  
+        }
+
+        
+    }
+}
